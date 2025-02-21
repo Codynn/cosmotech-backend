119b5ed46d2f89196990113426dfb26577acc566
Manoj Neupane <75156544+Manpane@users.noreply.github.com>
2025-02-21 22:21:44 +0545

    content distribution

---
+++ controllers/analytics.controller.js
@@ -0,0 +1,61 @@
+const { StatusCodes } = require("http-status-codes");
+const models = require("../models/index.model");
+
+const getContentCount = async (req, res) => {
+    const blogs = await models.blogModel.countDocuments();
+    const faqs = await models.faqModel.countDocuments();
+    const products = await models.productModel.countDocuments();
+    const reviews = await models.reviewModel.countDocuments();
+
+    return res.status(StatusCodes.OK).json({
+        blogs,
+        faqs,
+        products,
+        reviews,
+    });
+};
+
+const getProductsDistribution = async (req, res) => {
+    const products = await models.productModel.find({}).populate("categories");
+
+    let byCategory = {};
+    products.forEach((product) => {
+        if (!byCategory[product?.category?.name || "Uncategorized"]) {
+            byCategory[product?.category?.name || "Uncategorized"] = 0;
+        }
+        byCategory[product?.category?.name || "Uncategorized"]++;
+    });
+
+    return res.status(StatusCodes.OK).json({
+        success: true,
+        message: "Products by category",
+        data: byCategory,
+    });
+
+}
+
+const getBlogsDistribution = async (req, res) => {
+    const blogs = await models.blogModel.find({}).populate("category");
+
+    let byCategory = {};
+    blogs.forEach((blog) => {
+        if (!byCategory[blog?.category?.name || "Uncategorized"]) {
+            byCategory[blog?.category?.name || "Uncategorized"] = 0;
+        }
+        byCategory[blog?.category?.name || "Uncategorized"]++;
+    });
+
+    return res.status(StatusCodes.OK).json({
+        success: true,
+        message: "Blogs by category",
+        data: byCategory,
+    });
+
+}
+
+
+module.exports = {
+    getContentCount,
+    getProductsDistribution,
+    getBlogsDistribution,
+};
---
+++ routes/analytics.routes.js
@@ -0,0 +1,29 @@
+const { ROLES } = require('../constants/role.constants');
+const { verifyToken, checkRoles } = require('../middlewares/auth.middleware');
+const router = require('express').Router();
+require('express-async-errors');
+
+
+router.get(
+    "/content-count",
+    verifyToken,
+    checkRoles(ROLES.ADMIN),
+    getContentCount,
+);
+
+router.get(
+    "/products-distribution",
+    verifyToken,
+    checkRoles(ROLES.ADMIN),
+    getProductsDistribution,
+);
+
+router.get(
+    "/blogs-distribution",
+    verifyToken,
+    checkRoles(ROLES.ADMIN),
+    getBlogsDistribution,
+);
+
+
+module.exports = router;
